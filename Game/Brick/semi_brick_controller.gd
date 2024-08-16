78850d57c75e0f01d7f81131699d4e189e88aefa
dadames <dadames999@gmail.com>
2024-08-16 16:18:26 -0600

    Added globals and set up falling event to register semibricks to on brick controller

--- Game/Brick/semi_brick_controller.gd
+++ Game/Brick/semi_brick_controller.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 
 var brick: Brick
+var hasFallen := false
 
 
 func initialize(brickIn: Brick) -> void:
 	brick = brickIn
 	modulate = brick.color
+	brick.falling.connect(is_falling)
 
 func collided() -> void:
-	brick.start_falling()
+	if !hasFallen:
+		brick.start_falling()
+
+func is_falling() -> void:
+	hasFallen = true
