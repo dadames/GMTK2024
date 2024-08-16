fde62ed4021631e8a1491ee48d794fab205c3b5e
zedoom <harrydalbel@gmail.com>
2024-08-16 16:51:04 -0600

    ball now starts with a direction

--- Game/Ball/ball.gd
+++ Game/Ball/ball.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 1000
+@export var Speed:int = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2.DOWN * SPEED
+	#Find angle within a 60 degree downward cone
+#then use our angle to determine the direction ball starts
+	var angle:float = deg_to_rad(randf_range(60,120))
+	velocity = Vector2(cos(angle), sin(angle)) * Speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#Move the ball, check for collisions, and bounce off if a collision occurs.
 	var CollisionInfo := move_and_collide(velocity * delta)
 	if CollisionInfo:
 		velocity = velocity.bounce(CollisionInfo.get_normal())
- 
