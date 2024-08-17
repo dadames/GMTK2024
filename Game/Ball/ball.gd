399726184778c3950c8a6fe525c16c4105dfacec
dadames <dadames999@gmail.com>
2024-08-17 00:31:09 -0600

    Ball will bounce in direction of paddles movements

--- Game/Ball/ball.gd
+++ Game/Ball/ball.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
-@export var baseSpeed: float = 300
+@export var baseSpeed: float = 1000
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	#Find angle within a 60 degree downward cone
-	#then use our angle to determine the direction ball starts
 	var angle: float = deg_to_rad(randf_range(60,120))
 	velocity = Vector2(cos(angle), sin(angle)) * baseSpeed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	#Move the ball, check for collisions, and bounce off if a collision occurs.
-	var CollisionInfo := move_and_collide(velocity * delta)
-	if CollisionInfo:
-		var CollisionType := CollisionInfo.get_collider()
-		velocity = velocity.bounce(CollisionInfo.get_normal())
-		if CollisionType.has_method("collided"):
-			CollisionType.collided()
+	var collisionInfo := move_and_collide(velocity * delta)
+	if collisionInfo:
+		var collider := collisionInfo.get_collider()
+		velocity = velocity.bounce(collisionInfo.get_normal())
+		if collider.has_method("collided"):
+			collider.collided()
+		elif collider.is_class("CharacterBody2D"):
+			var paddle := collider as CharacterBody2D
+			var direction := 1 if paddle.velocity.x > 0 else -1 
+			velocity.x *= direction
