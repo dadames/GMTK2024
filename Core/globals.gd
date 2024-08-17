67eccf67a896de8ef2d7aa4e8192f7897d00cfa5
zedoom <harrydalbel@gmail.com>
2024-08-17 16:00:08 -0600

    Update globals.gd

--- Core/globals.gd
+++ Core/globals.gd
@@ -7,7 +7,8 @@ var LEVEL_SCALE: float = 1
 func _ready() -> void:
 	EventBus.level_started.connect(level_started)
 
+#Scale the grid for bricks snapping to paddle
 func level_started() -> void:
 	var TargetSize:int = Globals.LEVEL_SCALE
 	BLOCK_PIXELS = TargetSize * 32
-	print("scaling grid")
+	
