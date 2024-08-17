0d160c4219028fc2816a19e94752ac0041962364
Alex Sampley <alex.sampley@gmail.com>
2024-08-17 10:31:19 -0600

    Fixed double hitting of bricks on paddle

--- Game/Paddle/paddle.gd
+++ Game/Paddle/paddle.gd
@@ -17,6 +17,7 @@ var speed: float = INITIAL_SPEED
 @onready var bodies: Array[CollisionObject2D] = [%StaticBody2D, %CollisionDetection, self]
 @onready var base_collider: CollisionShape2D = %CollisionShape2D
 
+var _consumed_bricks_this_frame: Array[Brick] = []
 
 func _ready() -> void:
 	scale = Vector2(Globals.SCALE_MODIFIER, Globals.SCALE_MODIFIER)
@@ -44,23 +45,34 @@ func _physics_process(delta: float) -> void:
 	var halfSize: Vector2 = Vector2(get_viewport().size) / camera.zoom / 4.0
 	position.y = cameraPosition.y + (halfSize.y * 0.5)
 
-func consume_brick(brick: Brick, shift: Vector2i = Vector2i.UP + Vector2i.RIGHT) -> void:
+func consume_brick(brick: Brick, shift: Vector2i) -> void:
+	if brick in _consumed_bricks_this_frame:
+		return
+
+	print_debug(brick, shift)
+
+	_consumed_bricks_this_frame.append(brick)
+
 	# align the brick to the grid along shift
-	brick.position.x += shift.x * fmod(brick.position.x, Globals.BLOCK_PIXELS)# - Globals.BLOCK_PIXELS / 2.0
-	brick.position.y += shift.y * fmod(brick.position.x, Globals.BLOCK_PIXELS)# - Globals.BLOCK_PIXELS / 2.0
+	#var offset := shift as Vector2 / 2.0 * brick.position.posmod(Globals.BLOCK_PIXELS)
+	#brick.position += offset
+
 	for child: Node2D in brick.get_children():
 		if child is SemiBrick:
 			for brick_sprite: Sprite2D in child.find_children("Sprite2D"):
+				#brick_sprite.position += offset
 				brick_sprite.reparent(self)
 
 			for brick_collider: CollisionShape2D in child.find_children("CollisionShape2D"):
+				var dest_pos := brick_collider.global_position
+
 				for body in bodies:
 					var dup := brick_collider.duplicate()
 					body.add_child(dup)
-					dup.global_position = brick_collider.global_position
+					dup.global_position = dest_pos
 					dup.global_scale = brick_collider.global_scale
 
-	brick.call_deferred("queue_free")
+	brick.queue_free()
 
-func _on_area_2d_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
-	call_deferred("consume_brick", body.brick)
+func _on_collision_detection_body_entered(body: Node2D) -> void:
+	call_deferred("consume_brick", body.brick, (body.position - position).normalized().snappedf(1.0))
