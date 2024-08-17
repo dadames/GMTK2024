347226a31963705f25051f377f8e0ab242641383
Alex Sampley <alex.sampley@gmail.com>
2024-08-17 08:22:32 -0600

    Changed merge logic with paddle to duplicate shapes across all bodies, no need to wrangle hitboxes

--- Game/Brick/semi_brick_controller.gd
+++ Game/Brick/semi_brick_controller.gd
@@ -12,7 +12,6 @@ func initialize(brickIn: Brick) -> void:
 	brick = brickIn
 	sprite.modulate = brick.color
 	brick.falling.connect(on_falling)
-	brick.merge.connect(on_merge)
 
 func collided() -> void:
 	match activeState:
@@ -20,16 +19,8 @@ func collided() -> void:
 			brick.start_falling()
 		State.Falling:
 			brick.start_merge()
-		State.Merged:
-			get_parent().consume_brick()
 
 func on_falling() -> void:
 	activeState = State.Falling
 	set_collision_layer_value(4, false)
 	set_collision_layer_value(5, true)
-
-func on_merge() -> void:
-	activeState = State.Merged
-	set_collision_layer_value(5, true)
-	set_collision_layer_value(2, true)
-	set_collision_layer_value(7, true)
--- Game/Paddle/paddle.gd
+++ Game/Paddle/paddle.gd
@@ -10,12 +10,12 @@ var speed: float = INITIAL_SPEED
 	set(value):
 		width = value
 		sprite.texture.width = Globals.BLOCK_PIXELS * width
-		(collider.shape as RectangleShape2D).size.x = Globals.BLOCK_PIXELS * width
+		(base_collider.shape as RectangleShape2D).size.x = Globals.BLOCK_PIXELS * width
 		_ready()
 
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var body2D: PhysicsBody2D = $StaticBody2D
-@onready var collider: CollisionShape2D = %CollisionShape2D
+@onready var bodies: Array[CollisionObject2D] = [%StaticBody2D, %CollisionDetection, self]
+@onready var base_collider: CollisionShape2D = %CollisionShape2D
 
 
 func _ready() -> void:
@@ -41,8 +41,8 @@ func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta)
 	var camera := get_viewport().get_camera_2d()
 	var cameraPosition: Vector2 = camera.get_screen_center_position()
-	var halfSize: Vector2 = Vector2(get_viewport().size) / camera.zoom / Vector2(2, 2)
-	position.y = cameraPosition.y + (halfSize.y * 0.8)
+	var halfSize: Vector2 = Vector2(get_viewport().size) / camera.zoom / 4.0
+	position.y = cameraPosition.y + (halfSize.y * 0.5)
 
 func consume_brick(brick: Brick, shift: Vector2i = Vector2i.UP + Vector2i.RIGHT) -> void:
 	# align the brick to the grid along shift
@@ -50,10 +50,16 @@ func consume_brick(brick: Brick, shift: Vector2i = Vector2i.UP + Vector2i.RIGHT)
 	brick.position.y += shift.y * fmod(brick.position.x, Globals.BLOCK_PIXELS)# - Globals.BLOCK_PIXELS / 2.0
 	for child: Node2D in brick.get_children():
 		if child is SemiBrick:
-			var initialScale := child.global_scale
-			child.reparent(self)
-			child.global_scale = initialScale
-			child.collided()
+			for brick_sprite: Sprite2D in child.find_children("Sprite2D"):
+				brick_sprite.reparent(self)
+
+			for brick_collider: CollisionShape2D in child.find_children("CollisionShape2D"):
+				for body in bodies:
+					var dup := brick_collider.duplicate()
+					body.add_child(dup)
+					dup.global_position = brick_collider.global_position
+					dup.global_scale = brick_collider.global_scale
+
 	brick.call_deferred("queue_free")
 
 func _on_area_2d_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
