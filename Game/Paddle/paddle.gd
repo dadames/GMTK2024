3174a9e9339e9afb3260e377b56818891b19c034
zedoom <harrydalbel@gmail.com>
2024-08-17 22:24:59 -0600

    Update paddle.gd
    Documentation

--- Game/Paddle/paddle.gd
+++ Game/Paddle/paddle.gd
@@ -100,16 +100,15 @@ func on_level_started() -> void:
 	speed = 2 ** Globals.level_scale * initial_speed
 
 
-
+#Detect when a falling block hits and "catch" it if its hitting us from above
 func _on_collision_detection_body_shape_entered(body_rid:RID, body:Node2D, body_shape_index:int, local_shape_index:int) -> void:
 	print("detected")
 	#call_deferred("consume_brick", body.brick, Vector2.ZERO)
 	var Direction: Vector2 = (body.global_position - global_position).normalized()
 	var Side: float = Direction.angle_to(Vector2.RIGHT)
 	var Angle:int = rad_to_deg(Side)
-	
-	if Angle > 15 and Angle < 165 :
-		call_deferred("consume_brick", body.brick, Vector2.ZERO)
+	if Angle > 15 and Angle < 165 : #Defines what "above" means
+			call_deferred("consume_brick", body.brick, Vector2.ZERO)
 		#print("within angle. angle is:", Angle, " or ", Side)
 	#else:
 		#print("outside of angle. angle is:", Angle, " or ", Side)
