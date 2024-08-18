e3e54a1fb518a1b2c3c4c10881de33f01d52f6a7
Alex Sampley <alex.sampley@gmail.com>
2024-08-18 09:53:48 -0600

    Scale up speed of blocks falling

--- Core/globals.gd
+++ Core/globals.gd
@@ -3,6 +3,9 @@ extends Node
 const BLOCK_PIXELS := 32
 const SCALE_MODIFIER := 1#0.00001
 var level_scale: float = 1
+var level_factor: float:
+	get:
+		return 2 ** (level_scale - 1)
 
 func _ready() -> void:
 	EventBus.level_started.connect(level_started)
--- Game/Brick/semi_brick_controller.gd
+++ Game/Brick/semi_brick_controller.gd
@@ -36,7 +36,7 @@ func collided() -> void:
 
 func on_falling() -> void:
 	activeState = State.Falling
-	apply_central_impulse(Vector2(0,100))
+	apply_central_impulse(Vector2(0,100 * Globals.level_factor))
 	#set_collision_layer_value(4, false)
 	set_collision_layer_value(5, true)
 	set_collision_mask_value(6, false)
