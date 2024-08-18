496d2f0b8feb87ebc06d05d436850e06475c304e
dadames <dadames999@gmail.com>
2024-08-18 15:56:05 -0600

    Fixed web memory issues caused by massive song trying to play

---
+++ Audio/music_player.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+
+
+@export var soundtrack: Array[AudioStream]
+var currentIndex: int = 0
+
+
+func _ready() -> void:
+	stream = soundtrack[0]
+	play()
+
+func _on_finished() -> void:
+	currentIndex += 1
+	if currentIndex > soundtrack.size():
+		currentIndex = 0
+	stream = soundtrack[currentIndex]
+	play()
--- Game/debug_complete_level.gd
+++ Game/debug_complete_level.gd
@@ -1,6 +1,9 @@
 extends Button
 
 
+func _ready() -> void:
+	if !OS.is_debug_build():
+		hide()
 
 func _on_pressed() -> void:
 	EventBus.debug_complete_level.emit()
